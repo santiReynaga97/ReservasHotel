88ec1e83270758fb49d23b4a59bf51520c2818ce
santiReynaga97 <sreynaga@gsystems.com.ar>
2017-07-28 14:49:29 -0300

    creadas las funciones para agregar y borrar...  - simplificacion de codigo

--- Pruebas/js/archivo.js
+++ Pruebas/js/archivo.js
@@ -1,35 +1,40 @@
 
-function agreagarLi(id){
-  console.log(id);
-
-
-  if (id=='idBtnVendedor'){
-    console.log('holavendedor');
-    var texto=document.getElementById('txtVendedor').value;
-    var li=document.createElement('LI');
-    var liElementos= document.getElementById('listaVendedores').getElementsByTagName('li');
-    li.innerHTML=texto==''?'(nada)':texto;
-    document.getElementById('listaVendedores').appendChild(li).setAttribute("id", "idVendedor"+liElementos.length);
+function agregarLista(idTxt){
+  if (idTxt =='idBtnVendedor'){
+    agregar("txtVendedor","listaVendedores","Vendedor");
   }
   else {
-    console.log('holavendedor');
-    var texto=document.getElementById('txtProducto').value;
-    var li=document.createElement('LI');
-    var liElementos= document.getElementById('listaProducto').getElementsByTagName('li');
-    li.innerHTML=texto==''?'(nada)':texto;
-    document.getElementById('listaProducto').appendChild(li).setAttribute("id", "idProducto"+liElementos.length);
+    agregar("txtProducto","listaProducto","Producto");
   }
 }
 
-function borrar(){
-    lis=document.getElementById('listaVendedores').getElementsByTagName('li');
+function borrarLista(idLista){
+  if(idLista == 'listaVendedores'){
+    borrar(idLista);
+  }
+  else{
+    borrar(idLista);
+  }
+}
+
+
+function  agregar(texBox,lista,nombre){
+  var texto=document.getElementById(texBox).value;
+  var li=document.createElement('LI');
+  var liElementos= document.getElementById(lista).getElementsByTagName('li');
+  li.innerHTML=texto==''?'(nada)':texto;
+  document.getElementById(lista).appendChild(li).setAttribute("id", "id"+nombre+liElementos.length);
+}
+
+
+function borrar(lista){
+    lis=document.getElementById(lista).getElementsByTagName('li');
     console.log(lis);
     for(var i=0; i<lis.length;i++){
         lis[i].onclick=function(){
             if(confirm('¿Borrar este li?'))
           //  console.log(this);
             this.parentNode.removeChild(this);
-        };
+        }
     }
-
 }
